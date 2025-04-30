cbc93e79527bf1b7b29c1fc03c270c4f12b9d52d
Jardel Maldonado <114194617+JardelMaldonado@users.noreply.github.com>
2025-04-29 23:15:17 -0300

    Estrutura inicial da página de editar projetos
    style: adicionado título, botão de editar e detalhes do projeto na página de edição

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,27 +1,68 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
 
+import Loading from '../layouts/Loading'
+import Container from '../layouts/Container'
+import { func } from "prop-types"
+
 function Project() {
-     const {id} = useParams()
+  const { id } = useParams()
 
-      const [project, setProject] = useState([])
+  const [project, setProject] = useState([])
+  const [showProjectForm, setShowProjectForm] = useState(false)
 
-      useEffect(() => {
-         
-          fetch(`http://localhost:5000/projects/${id}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type' : 'application/json',
-            }
-          }).then((resp) => resp.json()).then((data) => {
-            setProject(data)
-          }).catch((err) => console.log(err))
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(`http://localhost:5000/projects/${id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then((resp) => resp.json()).then((data) => {
+        setProject(data)
+      }).catch((err) => console.log(err))
+    }, 500)
 
-      }, [id])
 
-    return (
-         <p>{project.name}</p>
-    )
+  }, [id])
+
+  function toggleProjectForm() {
+
+    setShowProjectForm(!showProjectForm)
+
+  }
+
+  return (
+    <>
+      {project.name ? (
+        <div className="p-4 w-full">
+          <Container className="flex flex-col w-full" >
+            <div className="w-full border-b border-[#7a7a7a] mb-[1.2em] pb-[1.2em]">
+              <div className="flex justify-between items-center mb-4">
+              <h1 className="text-2xl font-bold text-[#ffbb33] bg-[#222] mb-3 p-4">Projeto: {project.name}</h1>
+              <button className="bg-[#222] text-[#FFF]  py-[0.5em] px-[0.5em] transition duration-[0.5s] hover:text-[#ffbb33] border-none max-h-40 cursor-pointer" onClick={toggleProjectForm} >
+                {!showProjectForm ? 'Editar projeto' : 'Fechar'}
+              </button>
+              </div>
+              {!showProjectForm ? (
+                <div className="w-100 mt-4 space-y-1" >
+                  <p className="font-bold">Categoria: <span className="font-normal" >{project.category.name}</span></p>
+                  <p className="font-bold">Total de Orçamento: <span className="font-normal"> R${project.budget}</span></p>
+                  <p className="font-bold">Total de Utilizado: <span className="font-normal" > R${project.cost}</span></p>
+                </div>
+              ) : (
+                <div className="w-full mt-4">
+                  <p>detalhes do projeto</p>
+                </div>
+              )}
+            </div>
+          </Container>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
+  )
 }
 
-export default  Project
+export default Project
